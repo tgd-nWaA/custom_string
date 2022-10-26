68719871cb8e11b8918f36c3509f43a34b2ccaf5
tgd_nWaA <bohdan.buchok@ukma.edu.ua>
2022-10-26 16:51:11 +0300

    Inlined some functions, got rid of CopyOf fun, made some optimizations
    begin(), end(), getLength() and conversion operator to char* have been made inline

--- StringImplementation/String_Implementation/String_Implementation/String.cpp
+++ StringImplementation/String_Implementation/String_Implementation/String.cpp
@@ -4,9 +4,6 @@
 #include <iostream>
 #include <algorithm>
 
-using std::ostream;
-using std::out_of_range;
-
 String::String()
     : length(0), str(nullptr){}
 
@@ -18,48 +15,32 @@ String::String(const char _char)
 }
 
 String::String(const char* other)
-    : length(0), str(nullptr)
+    : String()
 {
-    copyOf(other);
-}
-
-String::String(const String& other)
-    : length(0), str(nullptr)
-{
-    copyOf(other.c_string());
-}
-
-//implement swap idiom
-String& String::operator=(const String& other)
-{
-    if (this != &other)
+    if (other != nullptr)
     {
-        cleanUp();
-        copyOf(other.c_string());
+        length = strlen(other);
+        str = new char[length + 1];
+        std::copy(other, other + length, str);
     }
-    return *this;
 }
+
+//check, not sure how it works
+String::String(const String& other)
+    : String(other.str){}
+
+//implemented copy-and-swap idiom
+ String& String::operator=(String other)
+ {
+     swap(*this, other);
+
+     return *this;
+ }
 
 String::String(String&& other) noexcept
+    : String()
 {
-    this->str = other.str;
-    this->length = other.length;
-    other.str = nullptr;
-    other.length = 0;
-}
-
-String& String::operator=(String&& other) noexcept
-{
-    if (this != &other)
-    {
-        cleanUp();
-        this->str = other.str;
-        this->length = other.length;
-        other.str = nullptr;
-        other.length = 0;
-    }
-
-    return *this;
+    swap(*this, other);
 }
 
 String::~String()
@@ -67,11 +48,10 @@ String::~String()
     cleanUp();
 }
 
-
 const char& String::operator[](const int i) const
 {
     if ( i < 0 || i >= length)
-        throw out_of_range("Out of bounds!");
+        throw std::out_of_range("Out of bounds!");
     
     return this->c_string()[i];
 }
@@ -79,7 +59,7 @@ const char& String::operator[](const int i) const
 char& String::operator[](int i)
 {
     if (i < 0 || i >= length)
-        throw out_of_range("Out of bounds!");
+        throw std::out_of_range("Out of bounds!");
     
     return this->c_string()[i];
 }
@@ -96,15 +76,9 @@ String String::operator+(const String& other) const
     return String(buffer);
 }
 
-bool String::operator==(const String& other) const
-{
-    if (other.c_string() == nullptr) return false;
-    return strcmp(str, other.str) == 0;
-}
-
-
 String& String::operator+=(const String& _other)
 {
+    //dumb
     if (this != &_other)
     {
         auto totalLength = length + _other.length;
@@ -123,41 +97,7 @@ String& String::operator+=(const String& _other)
     return *this;
 }
 
-//MyString MyString::operator + (const MyString& other)
-//{
-//    size_t targetLength = this->length + other.length;
-//    char* str = new char[targetLength + 1];
-//    memcpy(str, this->str, this->length);
-//    memcpy(str + this->length, other.str, other.length + 1);
-//    return MyString(str);
-//}
-
-String String::operator+(const char* _other) const
-{
-    const auto charLength = strlen(_other);
-    const auto totalLength = length + charLength;
-    char* buffer = new char[totalLength];
-
-    std::copy(str, str + length, buffer);
-    std::copy(_other, _other + charLength, buffer + length);
-    buffer[totalLength] = '\0';
-
-    return String(buffer);
-}
-
-
-void String::copyOf(const char* other)
-{
-    
-    if (other != nullptr)
-    {
-        length = strlen(other) + 1;
-        str = new char[length];
-        std::copy(other, other + length, str);
-    }
-}
-
-ostream& operator<<(std::ostream& _os, const String& _string)
+std::ostream& operator<<(std::ostream& _os, const String& _string)
 {
     _os << _string.c_string();
     return _os;
@@ -187,52 +127,33 @@ String String::toUpperCase() const
     return buffer;
 }
 
-size_t String::getLength() const
-{
-    return length;
-}
 
-char* String::c_string() const
-{
-    return str;
-}
 
-String::operator const char* () const
-{
-    return str;
-}
 
-//todo check for safety
-const char* String::begin()
+void String::swap(String& first, String& second) // nothrow
 {
-    return &this->c_string()[0];
-}
-
-//todo check for safety
-const char* String::end()
-{
-    return &this->c_string()[length];
-}
-
-void String::copyOf(const char* other)
-{
-    if (other != nullptr)
-    {
-        length = strlen(other) + 1;
-        str = new char[length];
-        std::copy(other, other + length, str);
-    }
+    // by swapping the members of two objects,
+    // the two objects are effectively swapped
+    std::swap(first.length, second.length);
+    std::swap(first.str, second.str);
 }
 
 void String::cleanUp()
 {
-    if (this->str != nullptr)
+    if (str != nullptr)
     {
         delete[] str;
+        str = nullptr;
     }
 
     length = 0;
-    str = nullptr;
 }
+
+bool String::operator==(const String& other) const
+{
+    if (other.c_string() == nullptr) return false;
+    return strcmp(str, other.str) == 0;
+}
+
 
 
--- StringImplementation/String_Implementation/String_Implementation/String.h
+++ StringImplementation/String_Implementation/String_Implementation/String.h
@@ -8,48 +8,73 @@ public:
     String(const char);
     String(const char*);
 
-    String(const String&);
-    String& operator=(const String&);
+    String(const String&) ;
+
+    //maybe we should make it private and not friend?
+  
+
+    String& operator=(String other);
+    //String& operator=(const String&);
 
     String(String&&) noexcept; //why noexcept here?
-    String& operator=(String&&) noexcept;
 
     ~String();
 
-    operator const char* () const; //how does it work?
-
     const char& operator[](const int) const;
     char& operator[](const int);
-
-    bool operator==(const String&) const;
     
-    String& operator+=(const String&);
-   
     String operator+(const String&) const;
-
+    String& operator+=(const String&);
 
     String toLowerCase() const;
     String toUpperCase() const;
 
-
     void reset();
 
-    
     const char* begin(); //should we have const here?
     const char* end(); // and here?
 
-    inline size_t getLength() const;
-    inline char* c_string() const; //why we don't have const after char* and how to make const char* 
+    //todo check for safety
+    const char* String::begin()
+    {
+        return &this->c_string()[0];
+    }
+
+    //todo check for safety
+    const char* String::end()
+    {
+        //[length + 1] as we assign in constructor length without accounting \0
+        return &this->c_string()[length];
+    }
+
+    inline size_t getLength() const
+    {
+        return length;
+    }
+
+    //how does it work?
+    explicit inline operator const char* () const
+    {
+        return str;
+    }
 
 private:
-    /*function used in converting
-    and copying constructor and
-    copying assignment operator*/
-    void copyOf(const char*);
+
+    //for copy-and-swap idiom implementation
+    void swap(String&, String&);
+    // deallocating memory and setting pointer and length to 0
     void cleanUp();
 
     size_t length;
     char* str;
 };
+
+
+bool operator == (const String& lstr, const String& rstr);
+bool operator != (const String& lstr, const String& rstr);
+bool operator >= (const String& lstr, const String& rstr);
+bool operator <= (const String& lstr, const String& rstr);
+bool operator >  (const String& lstr, const String& rstr);
+bool operator <  (const String& lstr, const String& rstr);
 
 std::ostream& operator<<(std::ostream& _os, const String& _string);
