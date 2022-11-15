4e4fe076a5033e15d9877ff2acaceaee9f9bf1eb
tgd_nWaA <bohdan.buchok@ukma.edu.ua>
2022-11-15 12:52:44 +0200

    tired commit

--- AnyString/foo.h
+++ AnyString/foo.h
@@ -1,8 +1,16 @@
 
-#include <memory>
+#define NDEBUG
+
 #include <string>
 
 class bad_index : std::exception {};
+class bad_pointer : std::exception {};
+
+template <typename CharT, typename Traits = std::char_traits<CharT>>
+class proxy;
+
+template <typename CharT, typename Traits = std::char_traits<CharT>>
+class string_rep;
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 class m_basic_string {
@@ -10,165 +18,378 @@ public:
 
     //default constructor
     m_basic_string()
-        : rep_(nullptr)
-    {}
+        : rep_(new string_rep(""))
+    { 
+        #ifndef NDEBUG
+        std::cout << "default constructor"
+        #endif
+    };
 
-    //convering constructor for c-style strings
+    //default constructor
+    m_basic_string(const CharT c)
+        : rep_(new string_rep(c))
+    {
+        #ifndef NDEBUG
+        std::cout << "converting constructor for one symbol of CharT "
+        #endif
+    };
+
+    //converting constructor for c-style strings 
     m_basic_string(const CharT* s)
-        : m_basic_string(s)
-    {};
+        : rep_(new string_rep(s))
+    {
+        #ifndef NDEBUG
+        std::cout << "converting constructor for c-style strings "
+        #endif
+    };
 
     //converting constructor for std::basic_string<CharT>
     m_basic_string(std::basic_string<CharT>& s)
-        : rep_(s);
-    {};
+        : m_basic_string(s.c_str())
+    {
+        #ifndef NDEBUG
+        std::cout << "converting constructor for std::basic_string<CharT>"
+        #endif  
+    };
 
-    //TODO how to get representation?
     //copy constructor
     m_basic_string(const m_basic_string& c)
-        : rep_(c.get_rep())
-    {};
-
-    //copy-and-swap implemented 
-    m_basic_string& operator=(m_basic_string other) &
-    {
-        swap(*this, other);
-
-        return *this;
-    }
-
-    //move constructor
-    m_basic_string (m_basic_string&& other) noexcept
         : m_basic_string()
     {
-        swap(*this, other);
+        
+        rep_ = c.rep_->get_copy();
+        #ifndef NDEBUG
+        std::cout << "copy constructor"
+        #endif  
+    };
+
+    m_basic_string& operator=(m_basic_string& s) &
+    {
+        ++s.rep_->use_count_;
+
+        if (--rep_->use_count_ == 0)
+            delete rep_;
+
+        rep_ = s.rep_;
+
+        return *this;
+    };
+
+    m_basic_string(m_basic_string&& s) noexcept
+        : m_basic_string()
+    {
+        std::swap(rep_, s.rep_);
+    }
+
+    m_basic_string& operator=(m_basic_string&& s) noexcept
+    {
+        ++s.rep_->use_count_;
+
+        if (--rep_->use_count_ == 0)
+            delete rep_;
+
+        rep_ = s.rep_;
+
+        return *this;
     }
 
     ~m_basic_string()
     {
-        clear();
-    }
+        /*clear();*/
+        
+        if (--rep_->use_count_ == 0) {
+            delete rep_;
+        }
 
-    m_basic_string& operator+=(const m_basic_string& str)&
-    {
+        #ifndef NDEBUG
+        std::cout << "destructor"
+        #endif  
+    };
+
+    m_basic_string& operator+=(const m_basic_string& s) & 
+    {        
+        
         size_t curr_len = len();
-        size_t add_len = str.len();
+        size_t add_len = s.len();
         size_t total_len = curr_len + add_len;
 
-        rep_ = new CharT[total_len + 1];
-        Traits::copy(res.get(), rep_.get(), curr_len);
-        Traits::copy(res.get() + curr_len, str.rep_.get(), add_len + 1);
+        CharT* s_res = new CharT[total_len + 1];
+        Traits::copy(s_res, rep_->str_, curr_len);
+        Traits::copy(s_res + curr_len, s.rep_->str_, add_len + 1);
+
+        string_rep* res(new string_rep(s_res));
+
+        if (--rep_->use_count_ == 0)
+            delete rep_;
 
         rep_ = res;
-        len_ = total_len;
-        return *this;
-    }
 
-    void clear()
-    {
-        rep_.reset();
-        len_ = 0;
-    }
+        return *this;
+    };
+
+    /*void clear(){};*/
 
     size_t len() const
     {
-        return len_;
-    }
+        return rep_->len();
+    };
 
     const CharT* c_str() const
     {
-        return rep_.get();
-    }
+        return rep_->c_str();
+    };
 
     bool empty() const
     {
-        return len() == 0;
-    }
+        return rep_->len() == 0;
+    };
 
-    proxy operator[](size_t i)
+    explicit inline operator const char* () const
     {
-        return proxy(this, i);
-    }
+        return c_str();
+    };
+
+    explicit inline operator 
+        std::basic_string<CharT>() const
+    {
+        return std::basic_string<CharT>(c_str());
+    };
+
+    //todo correct?
+    proxy<CharT> operator[](size_t i)
+    {
+        return proxy(rep_, i);
+    };
 
     CharT operator[](size_t i) const
     {
         if (i > len())
             throw bad_index();
-            
-        return *(rep_.get() + i);
-    }
+
+        return *(rep_ + i);
+    };
+
+    /*inline const char* begin() const
+    {
+        return &this->c_str()[0];
+    };
+
+    inline const char* end() const
+    {
+        return &this->c_str()[length + 1];
+    };*/
+
+    template <typename CharT, typename Traits>
+    class proxy
+    {
+    public:
+
+        proxy(string_rep<CharT>* rep, size_t pos)
+            : rep_(rep)
+            , pos_(pos)
+        {};
+
+        operator CharT()
+        {
+            if (pos_ > rep_->len())
+                throw bad_index();
+
+            return *(rep_ + pos_);
+        };
+
+        proxy operator= (const CharT& value)
+        {
+            if (pos_ > rep_->len())
+                throw bad_index();
+
+            if (rep_->use_count() == 1)
+                *(rep_ + pos_) = value;
+
+            if (rep_->use_count() != 1)
+            {
+                size_t l = rep_->len();
+                string_rep* res = string_rep<CharT>(new CharT[l + 1]);
+
+                Traits::copy(res, rep_->c_str(), l + 1);
+                *(res + pos_) = value;
+
+                //todo correct?
+                delete rep_;
+
+                rep_ = res;
+            }
+            return *this;
+        }
+
+    private:
+        string_rep<CharT>* rep_;
+        size_t        pos_;
+    };
 
 private:
 
-    /*void swap(m_basic_string& r)
+    /*void swap_with(m_basic_string& r)
     {
         std::swap(rep_, r.rep_);
-        std::swap(len_, r.len_);
-    }*/
+    };*/
 
-    class string_rep;
-    class proxy;
+    class string_rep {
+
+        friend class m_basic_string;
+    public:
+		//default constructor
+		string_rep(void)
+			: len_(0)
+			, str_(nullptr)
+			, use_count_(1)
+		{};
+
+        string_rep(const CharT c) 
+            : len_(1)
+            , str_(new CharT[2])
+            , use_count_(1)
+        {
+            *str_ = c; *(str_ + 1) = '\0';
+        };
+
+		//converting constructor for c-style strings
+		string_rep(const CharT* s)
+			: len_(Traits::length(s))
+			, str_(new CharT[len_ + 1])
+			, use_count_(1)
+		{
+			Traits::copy(str_, s, len_ + 1);
+		};
+
+		/*string_rep(std::string&);*/
+
+		~string_rep() {
+            delete str_;
+		};
+
+		//pseudo-copying constructor
+        string_rep* get_copy() {
+            ++use_count_;
+            return this;
+        };
+        
+		//pseudo-ajssignment
+        /*void assign(size_t len, const CharT* s) {
+            if (s == nullptr)
+                throw bad_pointer();
+            
+			CharT* res = new CharT[len + 1];
+            Traits::copy(res, s, len + 1);
+
+			
+            if (this->use_count_ == 1)
+                delete this;
+            else
+                --this->use_count_;
+
+
+
+
+            this(res);
+        };*/
+
+		string_rep(const string_rep* rep)
+			: len_(rep->len())
+			, str_(str_->c_str())
+			, use_count_(++rep->use_count())
+		{};
+
+        string_rep(const string_rep&) = delete;
+		string_rep& operator=(const string_rep&) = delete;
+
+		inline size_t use_count() const
+		{
+			return use_count_;
+		};
+
+		inline size_t len() const
+		{
+			return len_;
+		};
+
+		inline const char* c_str() const
+		{
+			return str_;
+		};
+
+    private:
+        size_t len_;
+        CharT* str_;
+        size_t use_count_;
+        /*bool shareable_;*/
+    };
 
     string_rep* rep_;
 };
+
+//template <typename CharT, typename Traits = std::char_traits<CharT>>
+//std::basic_ostream<CharT>&
+//operator<<(std::basic_ostream<CharT>& os, const proxy<CharT>& proxy)
+//{
+//    return os << str.c_str();
+//};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 std::basic_ostream<CharT>&
     operator<<(std::basic_ostream<CharT>& os, const m_basic_string<CharT>& str)
 {
     return os << str.c_str();
-}
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
-m_basic_string<CharT> operator+(const m_basic_string<CharT> l, const m_basic_string<CharT>& r)
+m_basic_string<CharT> operator+(m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
-    return std::move(l += r);
-}
+    return l += r;
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator==(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
     return (Traits::compare(l.c_str(), r.c_str(), l.len()) == 0);
-}
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator!=(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
     return !(l == (r));
-}
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator<(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
     return (Traits::compare(l.c_str(), r.c_str(), l.len()) < 0);
-}
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator>(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
-    return !(l<r);
-}
+    return !(l < r);
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator<=(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
-    return l < r || l==r;
-}
+    return l < r || l == r;
+};
 
 template <typename CharT, typename Traits = std::char_traits<CharT>>
 bool operator>=(const m_basic_string<CharT>& l, const m_basic_string<CharT>& r)
 {
-    return !(l < r) || l==r;
-}
+    return !(l < r) || l == r;
+};
 
 template <typename T, typename S>
-void swap(m_basic_string<T, S>& l, m_basic_string<T, S>& r)
+void swap_with(m_basic_string<T, S>& l, m_basic_string<T, S>& r)
 {
-    l.swap(r);
+    l.swap_with(r);
 }
 
 using m_string    = m_basic_string<char>;
 using wm_string   = m_basic_string<wchar_t>;
-using u8m_string  = m_basic_string<char8_t>;
 using u16m_string = m_basic_string<char16_t>;
 using u32m_string = m_basic_string<char32_t>;
--- AnyString/main.cpp
+++ AnyString/main.cpp
@@ -1,132 +1,174 @@
-//#include "String.h"
-//
-//void ColorPrint(const char* text, int fg_color, int bg_color);
-//
-//#include <iostream>
-//
-//int main(void) {
-//
-//	std::cout << "===================================" << "\n";
-//	std::cout << "default constructor" << "\n" << "\n";
-//	//default constructor
-//	String strEmpl;
-//	std::cout << strEmpl << "\n"; 
-//
-//	std::cout << "===================================" << "\n";
-//		std::cout << "converting constructor with char" << "\n" << "\n";
-//	//converting constructor with char
-//	String strEmpl1('a');
-//		std::cout << strEmpl1 << "\n"; 
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "converting constructors with char*" << "\n" << "\n";
-//	//converting constructor with char*
-//	String strEmpl2("Hallelujah");
-//		std::cout << strEmpl2 << "\n";
-//		
-//	String strEmpl4("GOD");
-//		std::cout << strEmpl4 << "\n"; 
-//
-//	const String strEmpl5("Maria");
-//		std::cout << strEmpl5 << "\n"; 
-//
-//		std::string fatherStr("Father");
-//	
-//		std::cout << "===================================" << "\n";
-//		std::cout << "converting constructor with std::string" << "\n" << "\n";
-//	//converting constructor with std::string
-//	String strEmpl6(fatherStr);
-//		std::cout << strEmpl6 << "\n"; 
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "	copying constructor" << "\n" << "\n";
-//	//copying constructor 
-//	String strEmpl3(strEmpl2); 
-//		std::cout << strEmpl3 << "\n";
-//	String strEmpl7(fatherStr);
-//		std::cout << strEmpl7 << "\n"; 
-//	String strEmpl8("Cappuchino");
-//		std::cout << strEmpl3 << "\n"; 
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "	assignment operator" << "\n" << "\n";
-//	//assignment operator
-//	strEmpl3 = strEmpl8;
-//		std::cout << strEmpl3 << "\n";
-//	strEmpl3 = strEmpl2 + strEmpl4;
-//		std::cout << strEmpl3 << "\n"; 
-//	strEmpl3 = "ARFA" + strEmpl4;
-//		std::cout << strEmpl3 << "\n"; 
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "	operator []" << "\n" << "\n";
-//	//operator []
-//	std::cout << strEmpl4[0] << "\n";
-//	strEmpl4[0] = 'N';
-//		std::cout << strEmpl4 << "\n";
-//	std::cout << strEmpl5[0] << "\n";
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "	operators += and +" << "\n" << "\n";
-//	//operators += and +
-//	strEmpl6 += "is" + strEmpl4;
-//		std::cout << strEmpl6 << "\n";
-//
-//		std::cout << "===================================" << "\n";
-//		std::cout << "	lower&upper case" << "\n" << "\n";
-//	//lower&upper case 
-//	std::cout << "\n";
-//	std::cout << strEmpl6.toLowerCase() << "\n";
-//	std::cout << strEmpl6.toUpperCase() << "\n";
-//
-//	std::cout << "===================================" << "\n";
-//		std::cout << "	equality operators" << "\n" << "\n";
-//	//eq operators
-//	std::cout << std::boolalpha;
-//	String com1("a"); String com2("b");
-//
-//	std::cout << (com1 == com2) << "\n";
-//	std::cout << (com1 != com2) << "\n";
-//	std::cout << (com1 >= com2) << "\n";
-//	std::cout << (com1 <= com2) << "\n";
-//	std::cout << (com1 > com2) << "\n";
-//	std::cout << (com1 < com2) << "\n";
-//	std::cout << std::noboolalpha << "\n";
-//
-//	std::cout << "===================================" << "\n";
-//
-//	String s("Tra-ta-ta");
-//	const char* ps = static_cast<const char*> (s);
-//	std::cout << (s += ps) << ';' << std::endl;
-//
-//	ColorPrint("some text", 91, 102);
-//	
-//	return 0;
-//}
-//
-//void ColorPrint(const char* text, int fg_color, int bg_color)
-//{
-//	static const char begin_sequence[]{ 0x1B,'[','\0' };
-//	static const char reset[]{ 0x1B,'[','0','m','\0' };
-//
-//	std::cout << begin_sequence << fg_color << ';' << bg_color << 'm' << text << reset;
-//}
 
+void ColorPrint(const char* text, int fg_color, int bg_color);
 
 #include "foo.h"
 
 #include <iostream>
-#include <string>
 
 int main(void) {
 
-
 	{
-		m_string foo1("Shmarkli");
+		std::cout << "===================================" << "\n";
+		std::cout << "default constructor" << "\n" << "\n";
+		m_string foo1;
 		std::cout << foo1 << "\n";
-
-		std::cout << "proxy: " << foo1[0] << "\n";
 	}
 
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "converting constructor with CharT" << "\n" << "\n";
+		m_string foo1('a');
+		std::cout << foo1 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "converting constructors with CharT*" << "\n" << "\n";
+		//converting constructor with char*
+		m_string foo1("Hallelujah");
+		std::cout << foo1 << "\n";
+
+		m_string  foo2("GOD");
+		std::cout << foo2 << "\n";
+
+		const m_string  foo3("Maria");
+		std::cout << foo3 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "converting constructor with std::string" << "\n" << "\n";
+
+		std::string foo1("Father");
+		m_string foo2(foo1);
+		std::cout << foo2 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "	copying constructor" << "\n" << "\n";
+
+		m_string foo1("Cappuchino");
+		std::cout << foo1 << "\n";
+		m_string foo2(foo1);
+		std::cout << foo2 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "operator =" << "\n" << "\n";
+
+		m_string foo1("Cappuchino");
+		std::cout << foo1 << "\n";
+		m_string foo2 = foo1;
+		std::cout << foo2 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << " operator +=" << "\n" << "\n";
+
+		m_string foo1("Kratos says:");
+		m_string foo2("\"ragnarok comes\"");
+
+		foo1 += foo2;
+		foo1 += " TODAY comes...either";
+
+		std::cout << foo1 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "operator + " << "\n" << "\n";
+
+		m_string foo1("Kratos says:");
+		m_string foo2("\"ragnarok comes\"");
+
+		m_string foo3("\"bob\"");
+
+		std::cout << foo2 + foo1 << "\n";
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "operator = & operator + " << "\n" << "\n";
+
+		m_string foo1("Kratos says:");
+		m_string foo2("\"ragnarok comes\"");
+
+		m_string foo3("\"bob\"");
+
+		foo3 = foo1 + foo2;
+		std::cout << foo3 << "\n";
+	}
+
+	{
+		m_string foo3("\"bob\"");
+
+		//TODO
+		/*foo3 = "ARFA" + foo3;
+		std::cout << foo3 << "\n";*/
+	}
+
+	{
+		std::cout << "===================================" << "\n";
+		std::cout << "	operator []" << "\n" << "\n";
+
+		/*m_string foo1("\"bob\"");
+		std::cout << foo1[0] << "\n";
+		foo1[0] = 'B';
+		std::cout << foo1 << "\n";*/
+
+		m_string foo1("\"bob\"");
+		foo1[0];
+		foo1[0] = 'B';
+		std::cout << foo1 << "\n";
+		
+	}
+
+	//	std::cout << "===================================" << "\n";
+	//	std::cout << "	operators += and +" << "\n" << "\n";
+	////operators += and +
+	//strEmpl6 += "is" + strEmpl4;
+	//	std::cout << strEmpl6 << "\n";
+
+	//	std::cout << "===================================" << "\n";
+	//	std::cout << "	lower&upper case" << "\n" << "\n";
+	////lower&upper case 
+	//std::cout << "\n";
+	//std::cout << strEmpl6.toLowerCase() << "\n";
+	//std::cout << strEmpl6.toUpperCase() << "\n";
+
+	//std::cout << "===================================" << "\n";
+	//	std::cout << "	equality operators" << "\n" << "\n";
+	////eq operators
+	//std::cout << std::boolalpha;
+	//String com1("a"); String com2("b");
+
+	//std::cout << (com1 == com2) << "\n";
+	//std::cout << (com1 != com2) << "\n";
+	//std::cout << (com1 >= com2) << "\n";
+	//std::cout << (com1 <= com2) << "\n";
+	//std::cout << (com1 > com2) << "\n";
+	//std::cout << (com1 < com2) << "\n";
+	//std::cout << std::noboolalpha << "\n";
+
+	//std::cout << "===================================" << "\n";
+
+	//String s("Tra-ta-ta");
+	//const char* ps = static_cast<const char*> (s);
+	//std::cout << (s += ps) << ';' << std::endl;
+
+	//ColorPrint("some text", 91, 102);
+	//
 	return 0;
-};
+}
+
+void ColorPrint(const char* text, int fg_color, int bg_color)
+{
+	static const char begin_sequence[]{ 0x1B,'[','\0' };
+	static const char reset[]{ 0x1B,'[','0','m','\0' };
+
+	std::cout << begin_sequence << fg_color << ';' << bg_color << 'm' << text << reset;
+}
+
